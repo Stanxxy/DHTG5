5115273a3179f4a549260f9f7d53d27d9abf1e43
Stanxxy <stan.x.liu.18@gmail.com>
2021-10-10 09:47:51 -0500

    update generate random function

--- src/com/company/Commands/GenerateRandomData.java
+++ src/com/company/Commands/GenerateRandomData.java
@@ -1,6 +1,12 @@
 package com.company.Commands;
 
+import com.company.Cassandra.CaCluster;
+import com.company.Commons.NodeCluster;
 import com.company.Main;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
 public class GenerateRandomData extends Command {
     public GenerateRandomData(Main main) {
@@ -15,10 +21,24 @@ public class GenerateRandomData extends Command {
         Integer x = Integer.parseInt(args[1]);
 
         System.out.println("Inserting " + x + " items...");
-        for(int i = 0; i < x; i ++) {
-            Long key = (long) i;
-            String value = "GENERATED ITEM " + i + "/" + x;
-            main.foregroundDHT.insert(key, value);
+
+        if(main.foregroundDHT.getName().equals("CaDHT")) {
+            Random r = new Random();
+            Set<Long> randomKeySet = new HashSet<>();
+            while(randomKeySet.size() < x) {
+                Long key = (long) (r.nextDouble() * CaCluster.getHashRange());
+                if(!randomKeySet.contains(key)){
+                    String value = "GENERATED ITEM " + key + "/" + x;
+                    randomKeySet.add(key);
+                    main.foregroundDHT.insert(key, value);
+                }
+            }
+        } else {
+            for(int i = 0; i < x; i ++) {
+                Long key = (long) i;
+                String value = "GENERATED ITEM " + i + "/" + x;
+                main.foregroundDHT.insert(key, value);
+            }
         }
     }
 
